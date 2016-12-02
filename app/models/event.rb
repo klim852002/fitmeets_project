585ebdc37e3ab6f9e65679a903862b4b71fc274b
Adrian Lee <leeadrian841@gmail.com>
2016-12-02 11:26:57 +0800

    update validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :comments
+
+  validates :postal_code, presence: true, length: { is: 6, message: "Please give a valid postal code" }, numericality: { only_integer: true, message: "Numbers only" }
+  validates :players_req, presence: true
 end
