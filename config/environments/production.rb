2175ac2224dac4f452f8f16401cbff6c52be571d
KUOPING LEE <blu_blac@hotmail.com>
2016-12-05 18:46:19 +0800

    changes to config environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  MyApp::Application.configure do
-    config.react.variant = :production
-  end
+
+  config.react.variant = :production
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
