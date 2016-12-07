6c406dc9abf644b7f67637c5dca7678ac40040df
Nurul Huda <huddyhuda@gmail.com>
2016-12-07 18:33:15 +0800

    new changes

--- app/assets/javascripts/components/event.es6.jsx
+++ app/assets/javascripts/components/event.es6.jsx
@@ -13,16 +13,17 @@ class Event extends React.Component {
         <h4><i className="fa fa-calendar maroon"></i>{this.props.event.event_date}</h4>
         <p>{this.props.event.details}</p>
         <p className="event-time"><i className="fa fa-clock-o maroon"></i> Start: {this.props.event.start_time}  End: {this.props.event.end_time} </p>
-        <p><i className="fa fa-map-marker maroon"></i> {this.props.event.event_address}</p>
+        <p><i className="fa fa-map-marker maroon"></i> {this.props.event.event_address}   <button id="mapBtn" type="button" className="see-map" data-toggle="modal" data-target="#eventMap">View Map</button> </p>
         <p>Postal Code: {this.props.event.postal_code}</p>
+          <a href={this.props.event.id + "/join"} data-method="post"><button className="draw">Join Event</button></a>
 
       </div>
       <div className="col-md-7 information-eventimage">
       { this.props.event.picture.url ?  <img src={this.props.event.picture.url}/> : <span className={`photo_${this.props.event.sports_cat.toLowerCase()}`}></span>}
 
           <p className="players-required">{this.props.event.players_req - this.props.eventusers.length} more players required</p>
-          <a href={this.props.event.id + "/join"} data-method="post"><button className="draw">Join Event</button></a>
-          <button id="mapBtn" type="button" className="draw" data-toggle="modal" data-target="#eventMap">See Map</button>
+
+
       </div>
 
 
