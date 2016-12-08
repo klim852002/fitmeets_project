09c12963a2bbc254498b74f99dd4eca138296543
KUOPING LEE <blu_blac@hotmail.com>
2016-12-08 15:08:09 +0800

    amended map location settings and added links to event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,29 @@
 class Event < ApplicationRecord
   mount_uploader :picture, PictureUploader
-  geocoded_by :event_address
+  # geocoded_by :event_address
+  geocoded_by :address_country
+
+  def address_country
+    [self.event_address].to_s + ' Singapore'
+  end
   # , :lookup => :region == "SG"
   # def event_address
-  #   [unit, street, sg].compact.join(', ')
+  #    Venue.near( params["event_address"] << ", Singapore" , 50)
+  # end
+  # Geocoder.search('event_address', :params => {:country => "sg"})
+
+  #   def event_address
+  #     "#{locale}, Singapore"
+  #   end
+
+  # geocoded_by :event_address do |prof,results|
+  #   if result = results.select{|res| res.country_code == "SG" }.first
+  #     unless (result.latitude.nil? || result.longitude.nil?)
+  #       prof.latitude = result.latitude
+  #       prof.longitude = result.longitude
+  #     end
+  #     result.coordinates
+  #   end
   # end
   after_validation :geocode
 
